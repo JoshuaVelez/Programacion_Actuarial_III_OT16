afc05c468c577c201208ecfa86e8e4fbc62315b0
Joshua Joan Velez Patron <joshuavpatron@gmail.com>
2016-08-31 09:46:40 -0500

    Avance de clase
    hasta el 31 de agosto

--- Avance Clase .R
+++ Avance Clase .R
@@ -123,3 +123,64 @@ class(x)
   rbind(m1,y)
   cbind(m2,y)
   
+  # Factores
+  x <- factor(c("si","no","si","si","no","si","no"))
+  x
+  table(x)
+  unclass(x)
+  x <- factor(c("si","no","si","si","no","si","no"),levels = c("si","no"))
+  x  
+  unclass(x)
+  
+  x <- factor(c("azul","azul","rojo","amarillo","verde","azul"))
+  x
+  table(x)
+  
+  # Valores Faltantes
+  x <- c(1,2,NA,10,3)
+  is.na(x)
+  is.nan(x)
+  
+  x <- c(1,2,NaN,10,3)
+  is.na(x)
+  is.nan(x)
+  
+  # Data Frames
+  x <- data.frame(Erick = 1:4, Lori = c(T,T,F,F))
+  x
+  row.names(x) <- c("Primero","segundo","tercero","cuarto")
+  x
+  nrow(x)
+  ncol(x)
+  attributes(x)
+  names(x) <- c("Yarely","Karen")
+  x
+  
+  # Los nombres no son exclusivos de los data frames
+  x <- 1:3
+  names(x) # vacio(null)
+  names(x) <- c("Hugo","Paco","Luis")
+  x
+  
+  x <- list(a=1:10,b=100:91,c=51:60)
+  x
+  names(x) <- c("Sec1","Sec2","Sec3")
+  x
+  
+  m <- matrix(1:4,2,2)
+  m
+  attributes(m)
+  dimnames(m) <- list(c("fill","fill2"),c("col1","col2"))
+  m
+  
+  m <- matrix(NA, 5,6)
+  dimnames(m) <- list (c(1:5), c("A","B","C","D","E","F"))
+  m
+ # Lectura de Datos
+  getwd()
+  setwd("~/GitHub/Programacion_Actuarial_III_OT16")
+  data <- read.csv("Datos_S&P.csv")
+  data <- read.table("Datos_S&P.csv",T,",")
+  data
+  data <- read.table("Datos_S&P.csv",F,",")
+  data
